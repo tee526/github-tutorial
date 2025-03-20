9d1ae622c73ffc85d9953121e56c7f97f34f12f2
tobi <adedotun599@gmail.com>
2025-03-20 14:32:27 +0100

    I AM PUSHING THIS

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ newElement.setAttribute('id','para-1');//set id
 newElement.setAttribute('class','class-s-m-x');
 let referenceElement =document.getElementById('p1');
 referenceElement.parentNode.insertBefore(newElement,referenceElement.nextSibling);
+
+
+
+
+function personalbar(fname, Lname) {
+    let firstName =fname;
+    let lastName=lname;
+  
+    let getDetails_noaccess = function () {
+      return (`First name is : ${firstname} Last
+  name is: ${lastname} `);
+    }
+  
+    this.getDetails_access = function () {
+      return (`first name is : ${firstname}, Last
+        name is: ${lastname}`);
+    }
+    }
+  
+    let personal=new persona ('mukul','latiyan');
+    console.log(persona1.firstname);
+    console.log(persona1.getDetails_noaccess);
+    console.log(persona1.getDetails_access());
